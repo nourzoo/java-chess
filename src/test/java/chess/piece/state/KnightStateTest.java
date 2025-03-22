f2c90a664ee1fa106bd21924e923117a587877cc
dltmddus <tmddus4671@gmail.com>
2025-03-22 17:25:09 +0900

    feat: 퀸 이동 기능 추가

--- src/test/java/chess/piece/state/KnightStateTest.java
+++ src/test/java/chess/piece/state/KnightStateTest.java
@@ -1,23 +1,43 @@
 package chess.piece.state;
 
-import static chess.Fixtures.B1;
-import static chess.Fixtures.C3;
+import static chess.Fixtures.B2;
+import static chess.Fixtures.C1;
+import static chess.Fixtures.C5;
+import static chess.Fixtures.D3;
+import static chess.Fixtures.E5;
+import static chess.Fixtures.F4;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.Movement;
+import chess.Position;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class KnightStateTest {
-    @Test
-    void 현재_위치에서_나이트가_이동할_위치까지_가는_길을_가져올_수_있다() {
+    @ParameterizedTest
+    @MethodSource("validParameters")
+    void 현재_위치에서_나이트가_이동할_위치까지_가는_길을_가져올_수_있다(Position beforePosition, Position afterPosition,
+                                              List<Movement> movements) {
         // given
         PieceState pieceState = new KnightState();
 
         // when
-        List<Movement> movablePath = pieceState.findMovablePath(B1, C3);
+        List<Movement> movablePath = pieceState.findMovablePath(beforePosition, afterPosition);
 
         // then
-        assertThat(movablePath).containsExactly(Movement.UP, Movement.UP, Movement.RIGHT);
+        assertThat(movablePath).isEqualTo(movements);
+    }
+
+    private static Stream<Arguments> validParameters() {
+        return Stream.of(
+                Arguments.of(D3, F4, List.of(Movement.RIGHT, Movement.RIGHT, Movement.UP)),
+                Arguments.of(D3, E5, List.of(Movement.UP, Movement.UP, Movement.RIGHT)),
+                Arguments.of(D3, C5, List.of(Movement.UP, Movement.UP, Movement.LEFT)),
+                Arguments.of(D3, B2, List.of(Movement.LEFT, Movement.LEFT, Movement.DOWN)),
+                Arguments.of(D3, C1, List.of(Movement.DOWN, Movement.DOWN, Movement.LEFT))
+        );
     }
 }
--- src/test/java/chess/piece/state/QueenStateTest.java
+++ src/test/java/chess/piece/state/QueenStateTest.java
@@ -1,9 +1,11 @@
 package chess.piece.state;
 
 import static chess.Fixtures.A3;
+import static chess.Fixtures.B1;
 import static chess.Fixtures.D1;
 import static chess.Fixtures.D3;
 import static chess.Fixtures.D6;
+import static chess.Fixtures.F5;
 import static chess.Fixtures.G3;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,12 +17,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class RookStateTest {
+public class QueenStateTest {
     @ParameterizedTest
     @MethodSource("validParameters")
-    void 현재_위치에서_룩이_이동할_위치까지_가는_길을_가져올_수_있다(Position beforePosition, Position afterPosition, List<Movement> movements) {
+    void 현재_위치에서_퀸이_이동할_위치까지_가는_길을_가져올_수_있다(Position beforePosition, Position afterPosition, List<Movement> movements) {
         // given
-        PieceState pieceState = new RookState();
+        PieceState pieceState = new QueenState();
 
         // when
         List<Movement> movablePath = pieceState.findMovablePath(beforePosition, afterPosition);
@@ -34,7 +36,9 @@ public class RookStateTest {
                 Arguments.of(D3, D1, List.of(Movement.DOWN, Movement.DOWN)),
                 Arguments.of(D3, D6, List.of(Movement.UP, Movement.UP, Movement.UP)),
                 Arguments.of(D3, G3, List.of(Movement.RIGHT, Movement.RIGHT, Movement.RIGHT)),
-                Arguments.of(D3, A3, List.of(Movement.LEFT, Movement.LEFT, Movement.LEFT))
+                Arguments.of(D3, A3, List.of(Movement.LEFT, Movement.LEFT, Movement.LEFT)),
+                Arguments.of(D3, F5, List.of(Movement.RIGHT_UP, Movement.RIGHT_UP)),
+                Arguments.of(D3, B1, List.of(Movement.LEFT_DOWN, Movement.LEFT_DOWN))
         );
     }
 }
